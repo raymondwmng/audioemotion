96b543ad827b4592ee650a6f7303e87af2c71936
Raymond Ng <raymond@emotech.co>
2018-10-08 11:01:38 +0000

    debug

--- preprocessing/CMU-MultimodalSDK/mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSEI_AUDIOONLY/__init__.py
+++ preprocessing/CMU-MultimodalSDK/mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSEI_AUDIOONLY/__init__.py
@@ -1,2 +1,2 @@
-from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSEI_AUDIOONLY import cmu_mosei_std_folds as standard_folds
+from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSEI_AUDIOONLY import cmu_mosei_audioonly_std_folds as standard_folds
 from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSEI_AUDIOONLY.cmu_mosei_audioonly import raw, highlevel, labels 
